9f816ba642ffaaa623a2797c83b67fad75fd9ef9
duedeu36 <duedeu36@gmail.com>
2019-05-26 23:48:13 +0200

    trying figure out issue wuith no src image

--- routes/books.js
+++ routes/books.js
@@ -3,39 +3,23 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/book");
-const multer = require("multer");
-const path = require("path"); // path + multer for upload neccessary (like coverImageBasePath in model)
-const fs = require("fs");
-const uploadPath = path.join("public", Book.coverImageBasePath);
 const Author = require("../models/author");
 const imageMimeTypes = ["images/jpeg", "images/png", "images/gif"]; // which files we only accept
-const upload = multer({
-  dest: uploadPath,
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter: (req, file, callback) => {
-    callback(null, imageMimeTypes);
-  }
-});
 
 // @route   GET books/
 // @desc    Get all books route
 // @access  Public
 router.get("/", async (req, res) => {
   let query = Book.find();
-  if (req.query.title !== null && req.query.title !== "") {
+  if (req.query.title != null && req.query.title != "") {
     query = query.regex("title", new RegExp(req.query.title, "i"));
   }
-  if (
-    req.query.publishedBefore1 !== null &&
-    req.query.publishedBefore1 !== ""
-  ) {
+  if (req.query.publishedBefore1 != null && req.query.publishedBefore1 != "") {
     // "publishDate" comes from the db
     // "lte" is from mongo and means less then (or) equal to
     query = query.lte("publishDate", req.query.publishedBefore1);
   }
-  if (req.query.publishedAfter2 !== null && req.query.publishedAfter2 !== "") {
+  if (req.query.publishedAfter2 != null && req.query.publishedAfter2 != "") {
     // "publishDate" comes from the db
     // "lte" is from mongo and means greater then (or) equal to
     query = query.gte("publishDate", req.query.publishedAfter2);
@@ -57,37 +41,25 @@ router.get("/new", async (req, res) => {
 });
 
 // @route   POST books/
-// @desc    Add book route
+// @desc    Create book route
 // @access  Public
-router.post("/", upload.single("cover"), async (req, res) => {
-  //   const fileName =
-  //     req.file != null ? req.file.filename : null;
-  const limits = req.file <= upload.limits ? req.file.filename : null;
+router.post("/", async (req, res) => {
   const book = new Book({
     title: req.body.title, // name comes from the input name="title" in _form_fields.ejs
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    coverImageName: limits,
     description: req.body.description
   });
+  saveCover(book, req.body.cover);
   try {
     const newBook = await book.save();
     // res.redirect(`books/${newBook.id}`)
     res.redirect(`books`);
   } catch (e) {
-    if (book.coverImageName != null) {
-      removeBookCover(book.coverImageName);
-    }
     renderNewPage(res, book, true);
   }
 });
-
-function removeBookCover(limits) {
-  fs.unlink(path.join(uploadPath, limits), err => {
-    if (err) console.error(err);
-  });
-}
 
 async function renderNewPage(res, book, hasError = false) {
   try {
@@ -103,4 +75,12 @@ async function renderNewPage(res, book, hasError = false) {
   }
 }
 
+function saveCover(book, coverEncoded) {
+  if (coverEncoded == null) return;
+  const cover = JSON.parse(coverEncoded);
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from(cover.data, "base64");
+    book.coverImageType = cover.type;
+  }
+}
 module.exports = router;
